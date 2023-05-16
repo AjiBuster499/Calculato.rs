8110277ecb7f1ccb163efd545be5926995d4f76c
Samuel A. Mikell <sbug1442@gmail.com>
2023-05-16 17:09:05 -0400

    Adjusted buttons

--- src/app.rs
+++ src/app.rs
@@ -1,6 +1,6 @@
 use iced::{
     executor,
-    widget::{button, column, container, row, text, vertical_rule, Text},
+    widget::{button, column, container, row, text, Text},
     window, Application, Command, Element, Theme,
 };
 
@@ -19,6 +19,7 @@ pub(crate) enum Message {
     Calculate,
     SendToEquation(String),
     Scientific,
+    Clear,
     Quit,
 }
 
@@ -48,10 +49,14 @@ impl Application for App {
     fn update(&mut self, message: Self::Message) -> Command<Message> {
         match message {
             Message::None => unimplemented!(),
-            Message::Calculate => self.calculator.calculate(),
+            Message::Calculate => {
+                self.calculator.push_to_equation(" )");
+                self.calculator.calculate();
+            }
             Message::SendToEquation(value) => self.calculator.push_to_equation(value.as_str()),
             Message::Quit => return window::close(),
             Message::Scientific => self.show_scientific = !self.show_scientific,
+            Message::Clear => self.calculator.clear(),
         };
 
         Command::none()
@@ -63,42 +68,40 @@ impl Application for App {
             equation,
             row![
                 button(text("Calculate")).on_press(Message::Calculate),
-                button(text("Quit")).on_press(Message::Quit),
+                button(text("Clear")).on_press(Message::Clear),
                 button(text("Scientific")).on_press(Message::Scientific),
+                button(text("Quit")).on_press(Message::Quit),
             ]
             .align_items(iced::Alignment::Center),
             row![
                 button(text("1")).on_press(Message::SendToEquation(String::from("1"))),
                 button(text("2")).on_press(Message::SendToEquation(String::from("2"))),
                 button(text("3")).on_press(Message::SendToEquation(String::from("3"))),
+                button(text("+")).on_press(Message::SendToEquation(String::from(" + "))),
             ]
             .align_items(iced::Alignment::Center),
             row![
                 button(text("4")).on_press(Message::SendToEquation(String::from("4"))),
                 button(text("5")).on_press(Message::SendToEquation(String::from("5"))),
                 button(text("6")).on_press(Message::SendToEquation(String::from("6"))),
+                button(text("-")).on_press(Message::SendToEquation(String::from(" - "))),
             ]
             .align_items(iced::Alignment::Center),
             row![
                 button(text("7")).on_press(Message::SendToEquation(String::from("7"))),
                 button(text("8")).on_press(Message::SendToEquation(String::from("8"))),
-                button(text("9")).on_press(Message::SendToEquation(String::from("9")))
+                button(text("9")).on_press(Message::SendToEquation(String::from("9"))),
+                button(text("*")).on_press(Message::SendToEquation(String::from(" * "))),
             ]
             .align_items(iced::Alignment::Center),
             row![
                 // TODO: Implement Negatives?
                 button(text("+/-")).on_press(Message::None),
                 button(text("0")).on_press(Message::SendToEquation(String::from("0"))),
-                button(text(".")).on_press(Message::SendToEquation(String::from(".")))
-            ]
-            .align_items(iced::Alignment::Center),
-            column![
-                button(text("+")).on_press(Message::SendToEquation(String::from(" + "))),
-                button(text("-")).on_press(Message::SendToEquation(String::from(" - "))),
-                button(text("*")).on_press(Message::SendToEquation(String::from(" * "))),
+                button(text(".")).on_press(Message::SendToEquation(String::from("."))),
                 button(text("/")).on_press(Message::SendToEquation(String::from(" / "))),
             ]
-            .align_items(iced::Alignment::Center)
+            .align_items(iced::Alignment::Center),
         ]
         .align_items(iced::Alignment::Center);
         container(content).into()
