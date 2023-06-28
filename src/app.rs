0db9fc4f1041de524b5521336d257bf3908d5a7c
Samuel A. Mikell <sbug1442@gmail.com>
2023-06-28 10:10:13 -0400

    Swapped keys for textInput. Kinks still to be worked out.

--- src/app.rs
+++ src/app.rs
@@ -1,10 +1,8 @@
 use iced::{
     alignment::{Alignment, Horizontal, Vertical},
     executor,
-    keyboard::{self, KeyCode},
-    subscription,
-    widget::{button, column, container, row, text},
-    window, Application, Command, Element, Event, Length, Theme,
+    widget::{button, column, container, row, text, text_input},
+    window, Application, Command, Element, Length, Theme,
 };
 
 use crate::parser::calculate;
@@ -17,7 +15,6 @@ pub(crate) struct App {
 pub(crate) enum Message {
     Calculate,
     SendToEquation(String),
-    Event(Event),
     Clear,
     Backspace,
     Exit,
@@ -54,138 +51,72 @@ impl Application for App {
             Message::Calculate => {
                 let answer = calculate(&self.display_equation);
                 self.display_equation = answer.to_string();
-                Command::none()
             }
             Message::SendToEquation(s) => {
                 self.display_equation.push_str(&s);
-                Command::none()
             }
-            Message::Exit => window::close(),
+            Message::Exit => return window::close(),
             Message::Clear => {
                 self.display_equation.clear();
-                Command::none()
             }
             Message::Backspace => {
                 self.display_equation.pop();
-                Command::none()
             }
-            // TODO: Find a better way to do this.
-            Message::Event(e) => {
-                if let Event::Keyboard(keyboard::Event::KeyPressed { key_code, .. }) = e {
-                    match key_code {
-                        KeyCode::Key1 | KeyCode::Numpad1 => {
-                            self.display_equation.push('1');
-                        }
-                        KeyCode::Key2 | KeyCode::Numpad2 => {
-                            self.display_equation.push('2');
-                        }
-                        KeyCode::Key3 | KeyCode::Numpad3 => {
-                            self.display_equation.push('3');
-                        }
-                        KeyCode::Key4 | KeyCode::Numpad4 => {
-                            self.display_equation.push('4');
-                        }
-                        KeyCode::Key5 | KeyCode::Numpad5 => {
-                            self.display_equation.push('5');
-                        }
-                        KeyCode::Key6 | KeyCode::Numpad6 => {
-                            self.display_equation.push('6');
-                        }
-                        KeyCode::Key7 | KeyCode::Numpad7 => {
-                            self.display_equation.push('7');
-                        }
-                        KeyCode::Key8 | KeyCode::Numpad8 => {
-                            self.display_equation.push('8');
-                        }
-                        KeyCode::Key9 | KeyCode::Numpad9 => {
-                            self.display_equation.push('9');
-                        }
-                        KeyCode::Key0 | KeyCode::Numpad0 => {
-                            self.display_equation.push('0');
-                        }
-                        KeyCode::Escape => {
-                            self.display_equation.clear();
-                        }
-                        KeyCode::Backspace => {
-                            self.display_equation.pop();
-                        }
-                        KeyCode::Enter
-                        | KeyCode::NumpadEnter
-                        | KeyCode::Equals
-                        | KeyCode::NumpadEquals => {
-                            let answer = calculate(&self.display_equation);
-                            self.display_equation = answer.to_string();
-                        }
-                        KeyCode::Plus | KeyCode::NumpadAdd => {
-                            self.display_equation.push('+');
-                        }
-                        KeyCode::Minus | KeyCode::NumpadSubtract => {
-                            self.display_equation.push('-');
-                        }
-                        KeyCode::Asterisk | KeyCode::NumpadMultiply => {
-                            self.display_equation.push('*');
-                        }
-                        KeyCode::Slash | KeyCode::NumpadDivide => {
-                            self.display_equation.push('/');
-                        }
-                        KeyCode::Period | KeyCode::NumpadDecimal => {
-                            self.display_equation.push('.');
-                        }
-                        _ => (),
-                    };
-                }
-                Command::none()
-            }
-        }
-    }
-
-    fn subscription(&self) -> iced::Subscription<Self::Message> {
-        subscription::events().map(Message::Event)
+        };
+        Command::none()
     }
 
     fn view(&self) -> Element<Message> {
-        let equation = text(&self.display_equation)
+        let equation_out = text(&self.display_equation)
             .horizontal_alignment(Horizontal::Center)
             .vertical_alignment(Vertical::Center)
             .width(Length::Fill);
+        let equation_in = text_input("Enter an equation...", "")
+            .on_input(Message::SendToEquation)
+            .on_submit(Message::Calculate);
         // Row containing the Equation
-        let eq_row = row![
-            equation,
-            button(text("X").horizontal_alignment(Horizontal::Center)).on_press(Message::Exit)
+        let eq_row = column![
+            equation_out,
+            row![
+                equation_in,
+                button(text("X").horizontal_alignment(Horizontal::Center)).on_press(Message::Exit)
+            ]
         ];
         // Scientific Functions
-        let scientific_block = column![row![
-            button(text("^").horizontal_alignment(Horizontal::Center))
-                .on_press(Message::SendToEquation("^".to_string()))
-                .width(Length::Fill),
-            button(text("^2").horizontal_alignment(Horizontal::Center))
-            .on_press(Message::SendToEquation("^2".to_string()))
-            .width(Length::Fill),
-            button(text("log").horizontal_alignment(Horizontal::Center))
-                .on_press(Message::SendToEquation("log(".to_string()))
-                .width(Length::Fill),
-            button(text("ln").horizontal_alignment(Horizontal::Center))
-                .on_press(Message::SendToEquation("ln(".to_string()))
-                .width(Length::Fill),
-            button(text("!").horizontal_alignment(Horizontal::Center))
-                .on_press(Message::SendToEquation("!".to_string()))
-                .width(Length::Fill),
+        let scientific_block = column![
+            row![
+                button(text("^").horizontal_alignment(Horizontal::Center))
+                    .on_press(Message::SendToEquation("^".to_string()))
+                    .width(Length::Fill),
+                button(text("^2").horizontal_alignment(Horizontal::Center))
+                    .on_press(Message::SendToEquation("^2".to_string()))
+                    .width(Length::Fill),
+                button(text("log").horizontal_alignment(Horizontal::Center))
+                    .on_press(Message::SendToEquation("log(".to_string()))
+                    .width(Length::Fill),
+                button(text("ln").horizontal_alignment(Horizontal::Center))
+                    .on_press(Message::SendToEquation("ln(".to_string()))
+                    .width(Length::Fill),
+                button(text("!").horizontal_alignment(Horizontal::Center))
+                    .on_press(Message::SendToEquation("!".to_string()))
+                    .width(Length::Fill),
+            ]
+            .align_items(Alignment::Center),
+            row![
+                button(text("(").horizontal_alignment(Horizontal::Center))
+                    .on_press(Message::SendToEquation("(".to_string()))
+                    .width(Length::Fill),
+                button(text(")").horizontal_alignment(Horizontal::Center))
+                    .on_press(Message::SendToEquation(")".to_string()))
+                    .width(Length::Fill),
+                button(text("nPr").horizontal_alignment(Horizontal::Center))
+                    .on_press(Message::SendToEquation("nPr".to_string()))
+                    .width(Length::Fill),
+                button(text("nCr").horizontal_alignment(Horizontal::Center))
+                    .on_press(Message::SendToEquation("nCr".to_string()))
+                    .width(Length::Fill),
+            ],
         ]
-        .align_items(Alignment::Center),
-        row![
-            button(text("(").horizontal_alignment(Horizontal::Center))
-            .on_press(Message::SendToEquation("(".to_string()))
-            .width(Length::Fill),
-            button(text(")").horizontal_alignment(Horizontal::Center))
-            .on_press(Message::SendToEquation(")".to_string()))
-            .width(Length::Fill),
-            button(text("nPr").horizontal_alignment(Horizontal::Center))
-            .on_press(Message::SendToEquation("nPr".to_string()))
-            .width(Length::Fill),
-            button(text("nCr").horizontal_alignment(Horizontal::Center))
-            .on_press(Message::SendToEquation("nCr".to_string()))
-            .width(Length::Fill),
-        ],]
         .align_items(Alignment::Center);
         let content = column![
             eq_row,
